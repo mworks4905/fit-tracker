1cbed0a5969615527fa5d2b97141c20e77437585
Lisa Ma <limawebdev@gmail.com>
2016-10-09 16:49:08 -0600

    JK now it's fully routed

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,7 @@ var router = express.Router();
 var cookieSession = require('cookie-session');
 
 router.get('/', (req, res, next) => {
-  res.render('../public/index.html');
+  res.redirect('index')
 })
-
-router.post('/', (req, res, next) => {
-  if(req.body.email === '' || req.body.password === ''){
-    res.send('Error!!!')
-  }else{
-    res.end();
-  }
-});
 
 module.exports = router;
