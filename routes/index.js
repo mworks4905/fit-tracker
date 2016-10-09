1dea8b05701b8fcf9cf9dabf61fc737db1391505
Lisa Ma <limawebdev@gmail.com>
2016-10-09 16:04:04 -0600

    Made some edits to index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var cookieSession = require('cookie-session');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('../public/index.html');
+})
+
+router.post('/', (req, res, next) => {
+  if(req.body.email === '' || req.body.password === ''){
+    res.send('Error!!!')
+  }else{
+    res.end();
+  }
 });
 
 module.exports = router;
