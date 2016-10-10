d6c7f5c94a436092877e18f120d7e77c9b67ad63
Matt Works <mworks4905@gmail.com>
2016-10-09 18:44:20 -0600

    Add day.hbs file

--- routes/day.js
+++ routes/day.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex');
+var knex = require('../knex');
 
 // Total User points
 router.get('/', function(req, res, next) {
@@ -26,32 +26,29 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/', (req, res, next) => {
-  // check current time
-  function localTime(){
-    var timeInMs = Date.now()
-    var time = new Date(timeInMs);
-    var hours = time.getHours();
-    return hours
-  }
-  console.log(hours)
+// router.get('/', (req, res, next) => {
+//   // check current time
+//   var localTime = () => {
+//     var timeInMs = Date.now();
+//     var time = new Date(timeInMs);
+//     var hours = time.getHours();
+//     return hours;
+//   };
+//   console.log(localTime());
+//
+//   if(hours >= 0 && hours < 11){
+//     res.send('morning')//replace with morning tab image
+//   };
+//   else if(hours >= 11 && hours < 14){
+//     res.send('afternoon')//replace with afternoon tab image
+//   };
+//   else{
+//     res.send('evening')//replace with evening tab image
+//   };
+// });
 
-  if(hours >= 0 && hours < 11){
-    res.send('morning')//replace with morning tab image
-  };
-  else if(hours >= 11 && hours < 14){
-    res.send('afternoon')//replace with afternoon tab image
-  };
-  else{
-    res.send('evening')//replace with evening tab image
-  };
-  // pull up day part tabs (one graphic for each day part with that specific time of day grayed out)
-  // send that graphic
-  setTimeout((localTime) => {
-  
-  }, 1000)
-  console.log(localTime);
-
-});
+// router.post('/', (req, res, next) => {
+//
+// })
 
 module.exports = router;
