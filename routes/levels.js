50864a9032ea08a1a01b5081dc2519afa5872a9a
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-10 10:35:14 -0600

    hbs

--- routes/levels.js
+++ routes/levels.js
@@ -3,27 +3,32 @@ var router = express.Router();
 const knex = require('../knex');
 
 /* GET users listing. */
-router.patch('/:id', function(req, res, next) {
-  knex('users').where('users.id', req.params.id).first().then((person) {
-    if (!person) {
-      const err = new Error('body.id does not exist');
-      err.status = 400;
-      throw err;
-    }
-    return knex('users')
-      .where('id', req.params.id)
-      .update({
-        tot_pts:
-        lvl:
-        email:
-      }, '*')
-  })
-  .then((books) => {
-    res.send(humps.camelizeKeys(books[0]));
-   })
-    .catch((err) => {
-     next(err);
-   });
+
+router.get('/', function(req, res, next) {
+  res.render('levels')
+});
+
+router.post('/update-level/:id', function(req, res, next) {
+  console.log("hitting update route");
+  // knex('users').where('users.id', req.params.id).first().then((person) {
+  //   if (!person) {
+  //     const err = new Error('body.id does not exist');
+  //     err.status = 400;
+  //     throw err;
+  //   }
+  //   return knex('users')
+  //     .where('id', req.params.id)
+  //     .update({
+  //       tot_pts:
+  //       lvl:
+  //     }, '*')
+  // })
+  // .then((books) => {
+  //   res.send(humps.camelizeKeys(books[0]));
+  //  })
+  //   .catch((err) => {
+  //    next(err);
+  //  });
 });
 
 module.exports = router;
