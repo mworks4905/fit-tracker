1b0daef6cd310819f774fb6a49979c589f545d00
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-10 09:27:41 -0600

    monday commit

--- routes/levels.js
+++ routes/levels.js
@@ -1,29 +1,32 @@
-// var express = require('express');
-// var router = express.Router();
-// const knex = require('../knex');
-//
-// /* GET users listing. */
-// router.patch('/:id', function(req, res, next) {
-//   knex('users').where('users.id', req.params.id).first().then((person) {
-//     if (!person) {
-//       const err = new Error('body.id does not exist');
-//       err.status = 400;
-//       throw err;
-//     }
-//     return knex('users')
-//       .where('id', req.params.id)
-//       .update({
-//         title: req.body.title,
-//         author: req.body.author,
-//         genre: req.body.genre,
-//         description: req.body.description,
-//         cover_url: req.body.coverUrl
-//       }, '*')
-//   })
-//
-// });
-//
-// module.exports = router;
+var express = require('express');
+var router = express.Router();
+const knex = require('../knex');
+
+/* GET users listing. */
+router.patch('/:id', function(req, res, next) {
+  knex('users').where('users.id', req.params.id).first().then((person) {
+    if (!person) {
+      const err = new Error('body.id does not exist');
+      err.status = 400;
+      throw err;
+    }
+    return knex('users')
+      .where('id', req.params.id)
+      .update({
+        tot_pts:
+        lvl:
+        email:
+      }, '*')
+  })
+  .then((books) => {
+    res.send(humps.camelizeKeys(books[0]));
+   })
+    .catch((err) => {
+     next(err);
+   });
+});
+
+module.exports = router;
 
 
 // router.patch('/:id', (req, res, next) => {
