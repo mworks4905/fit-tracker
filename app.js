6137f54f8b5ec6e102fb7ae8a75b8a59363b527d
Lisa Ma <limawebdev@gmail.com>
2016-10-11 10:01:38 -0600

    added listening on port stuff

--- app.js
+++ app.js
@@ -75,5 +75,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
 
 module.exports = app;
