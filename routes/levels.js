9a5bf8749b8ee21dc92b7c709782475cadf964c2
ambaldwin21 <anna.baldwin21@gmail.com>
2016-10-11 19:32:28 -0600

    working api calls onclick

--- routes/levels.js
+++ routes/levels.js
@@ -5,33 +5,34 @@ const knex = require('../knex');
 /* GET users listing. */
 
 router.get('/', (req, res, next) => {
-  res.render('levels')
+    res.render('levels')
 });
 
 router.put('/', (req, res, next) => {
-  // console.log('doing the put!')
-  let level = req.body.level
-  let points = 0
-  console.log(req.body)
-  if(level === '1') {
-    points = 500
-  } else if (level === '2') {
-    points = 250
-  } else {
-    points = 0
-  }
-  return knex('users')
-    .where('users.id', req.session.userInfo.id)
-    .update({
-      tot_pts: points,
-      lvl: level}, '*')
+    // console.log('doing the put!')
+    let level = req.body.level
+    let points = 0
+    console.log(req.body)
+    if (level === '1') {
+        points = 500
+    } else if (level === '2') {
+        points = 250
+    } else {
+        points = 0
+    }
+    return knex('users')
+        .where('users.id', req.session.userInfo.id)
+        .update({
+            tot_pts: points,
+            lvl: level
+        }, '*')
 
-  .then(() => {
-    res.end();
-   })
-    .catch((err) => {
-     next(err);
-   });
+    .then(() => {
+            res.end();
+        })
+        .catch((err) => {
+            next(err);
+        });
 });
 
 
